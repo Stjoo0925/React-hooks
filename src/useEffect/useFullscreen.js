41b58e2ddf9d3c80c8ff112dcdcfb81c793ecc77
Stjoo0925 <stjoo0925@gmail.com>
2024-10-21 00:01:04 +0900

    lecture: 2.6

---
+++ src/useEffect/useFullscreen.js
@@ -0,0 +1,18 @@
+export const useFullscreen = () => {
+  const element = useRef();
+  const triggerFull = (callback) => {
+    if (element.current) {
+      element.current.requestFullscreen();
+      if (callback && typeof callback === "function") {
+        callback(true);
+      }
+    }
+  };
+  const exitFull = (callback) => {
+    document.exitFullscreen();
+    if (callback && typeof callback === "function") {
+      callback(false);
+    }
+  };
+  return { element, triggerFull, exitFull };
+};
