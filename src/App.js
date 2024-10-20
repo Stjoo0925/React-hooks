498658dc96f0f81826575afd6a841f0944a193f5
Stjoo0925 <stjoo0925@gmail.com>
2024-10-20 18:44:58 +0900

    lecture: 2.3

--- src/App.js
+++ src/App.js
@@ -1,26 +1,9 @@
 import React, { useEffect, useRef } from "react";
 
-const useClick = (onClick) => {
-  const element = useRef();
-  useEffect(() => {
-    if (element.current) {
-      element.current.addEventListener("click", onClick);
-    }
-    return () => {
-      if (element.current) {
-        element.current.removeEventListener("click", onClick);
-      }
-    };
-  }, []);
-  return element;
-};
-
 const App = () => {
-  const sayHello = () => console.log("say Hello");
-  const title = useClick(sayHello);
   return (
     <div className="App">
-      <h1 ref={title}>Hi</h1>
+      <h1>Hi</h1>
     </div>
   );
 };
